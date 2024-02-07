81fbe04f84ab124fd6f75bfee7a01a97f75b93b2
Heiser <heiser9295@hotmail.com>
2024-02-07 16:36:04 +0800

    add more test

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -2,10 +2,12 @@ package lexer
 
 import "github.com/heiku-jiqu/go-interpreter/token"
 
-type Lexer struct{}
+type Lexer struct {
+	sourceCode string
+}
 
 func NewLexer(s string) *Lexer {
-	return &Lexer{}
+	return &Lexer{s}
 }
 
 func (l *Lexer) NextToken() token.Token {
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -25,4 +25,25 @@ func TestLexing(t *testing.T) {
 			}
 		}
 	})
+	t.Run("test symbol tokens", func(t *testing.T) {
+		input := "=+{}(),;"
+		lexer := NewLexer(input)
+
+		expected := []token.Token{
+			{Type: token.ASSIGN, Literal: "="},
+			{Type: token.PLUS, Literal: "+"},
+			{Type: token.LBRACE, Literal: "{"},
+			{Type: token.RBRACE, Literal: "}"},
+			{Type: token.LPAREN, Literal: "("},
+			{Type: token.RPAREN, Literal: ")"},
+			{Type: token.COMMA, Literal: ","},
+			{Type: token.SEMICOLON, Literal: ";"},
+		}
+		for _, expectTok := range expected {
+			tok := lexer.NextToken()
+			if tok != expectTok {
+				t.Fatalf("expected %+v but got %+v", expectTok, tok)
+			}
+		}
+	})
 }
